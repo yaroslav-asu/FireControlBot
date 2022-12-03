f45d60c7a04db494136dc28a7a9d8a464f542e78
Fizic <kirya.firsov.04@gmail.com>
2022-12-03 19:07:50 +0300

    feat: inlines

--- bot/handlers/core.py
+++ bot/handlers/core.py
@@ -1,3 +1,4 @@
 from bot.handlers.help import *
 from bot.handlers.start import *
 from bot.handlers.message import *
+from bot.handlers.inline import *
--- bot/main.py
+++ bot/main.py
@@ -3,7 +3,14 @@ from telebot import custom_filters
 from bot.handlers.core import *
 from bot.notifications.main import setup_notifications
 
-
+@bot.inline_handler(lambda query: query.query == 'text')
+def query_text(inline_query):
+    try:
+        r = types.InlineQueryResultArticle('1', 'Result', types.InputTextMessageContent('Result message.'))
+        r2 = types.InlineQueryResultArticle('2', 'Result2', types.InputTextMessageContent('Result message2.'))
+        bot.answer_inline_query(inline_query.id, [r, r2])
+    except Exception as e:
+        print(e)
 
 
 bot.add_custom_filter(custom_filters.StateFilter(bot))
